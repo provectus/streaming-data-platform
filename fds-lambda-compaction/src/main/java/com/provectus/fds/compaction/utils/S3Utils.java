bc5c4d7de948ff160783e6a150089d7b0710e45e
Rustam Gimadiev <gimadiev.kzn@yandex.ru>
2018-12-18 09:44:37 +0300

    glue table fix, decompression added

--- fds-lambda-compaction/src/main/java/com/provectus/fds/compaction/utils/S3Utils.java
+++ fds-lambda-compaction/src/main/java/com/provectus/fds/compaction/utils/S3Utils.java
@@ -6,12 +6,15 @@ import org.apache.hadoop.fs.Path;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.IOException;
+import java.util.zip.GZIPInputStream;
 
 public class S3Utils {
     public static File downloadFile(S3Object object) throws IOException {
         File outputFile = File.createTempFile(object.getBucketName(), object.getKey().replace(Path.SEPARATOR, "_"));
-        try (S3ObjectInputStream s3is = object.getObjectContent();
+        try (S3ObjectInputStream s3gs = object.getObjectContent();
+             InputStream s3is = new GZIPInputStream(s3gs);
              FileOutputStream fos = new FileOutputStream(outputFile)) {
             byte[] read_buf = new byte[1024];
             int read_len = 0;
