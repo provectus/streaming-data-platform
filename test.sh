dc08d5b8ed0a553673e40edecd293bd0f6ccb385
RustamGimadiev <gimadiev.kzn@yandex.ru>
2018-12-20 16:33:44 +0300

    fix test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-REST_ID=$2
+REST_ID=`aws apigateway get-rest-apis | jq -r '.items[] | select(.name="fdsanalytics_FDS_API").id'`
 CID=$RANDOM
 
 resources=`aws apigateway get-resources --rest-api-id ${REST_ID}`
@@ -11,7 +11,7 @@ click_resource=`echo $resources | jq -r '.items[] | select(.path | contains("cli
 for i in `seq 1 $1`
 	do
 	txid=$RANDOM
-	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${bid_resource} --path-with-query-string /bids?appuid=123\&txid=${txid}\&campaign_item_id=${CID}
+	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${bid_resource} --path-with-query-string /bids?appuid=123\&txid=${txid}\&campaign_item_id=${CID}\&domain=example.com\&creative_category=test\&creative_id=some
 	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${impression_resource} --path-with-query-string /impressions?appuid=123\&txid=${txid}\&win_price=100
-#	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${click_resource} --path-with-query-string /clicks?appuid=123\&txid=${txid}\&campaign_item_id=${CID}
+	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${click_resource} --path-with-query-string /clicks?appuid=123\&txid=${txid}\&campaign_item_id=${CID}
 done
