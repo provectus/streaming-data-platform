35b1e3c4d6fdeec97e91cb42ea7a965aa526d2e9
RustamGimadiev <gimadiev.kzn@yandex.ru>
2018-12-24 17:23:02 +0300

    crawler and deployment for API added

--- test.sh
+++ test.sh
@@ -1,17 +1,12 @@
 #! /bin/bash
 
-REST_ID=`aws apigateway get-rest-apis | jq -r '.items[] | select(.name="'$1'_FDS_API").id'`
 CID=$RANDOM
-
-resources=`aws apigateway get-resources --rest-api-id ${REST_ID}`
-bid_resource=`echo $resources | jq -r '.items[] | select(.path | contains("bids")).id'`
-impression_resource=`echo $resources | jq -r '.items[] | select(.path | contains("impressions")).id'`
-click_resource=`echo $resources | jq -r '.items[] | select(.path | contains("clicks")).id'`
+BASE_URL=`aws cloudformation  describe-stacks --stack-name fds-$1 | jq -r '.Stacks[0].Outputs[] | select(.OutputKey | contains("RootUrl")).OutputValue'`
 
 for i in `seq 1 $2`
 	do
 	txid=$RANDOM
-	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${bid_resource} --path-with-query-string /bids?appuid=123\&txid=${txid}\&campaign_item_id=${CID}\&domain=example.com\&creative_category=test\&creative_id=some
-	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${impression_resource} --path-with-query-string /impressions?appuid=123\&txid=${txid}\&win_price=100
-	aws apigateway test-invoke-method --rest-api-id=${REST_ID} --http-method=GET --resource-id=${click_resource} --path-with-query-string /clicks?appuid=123\&txid=${txid}\&campaign_item_id=${CID}
+	curl -v $BASE_URL/bids?appuid=123\&txid=${txid}\&campaign_item_id=${CID}\&domain=example.com\&creative_category=test\&creative_id=some
+	curl -v $BASE_URL/impressions?appuid=123\&txid=${txid}\&win_price=100
+	curl -v $BASE_URL/clicks?appuid=123\&txid=${txid}\&campaign_item_id=${CID}
 done
