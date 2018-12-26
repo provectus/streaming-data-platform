56bad7eb8d6b76292c9abb80bc287be526432590
RustamGimadiev <gimadiev.kzn@yandex.ru>
2018-12-26 19:45:29 +0300

    fix policy

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 CID=$RANDOM
-BASE_URL=`aws cloudformation  describe-stacks --stack-name fds$1 | jq -r '.Stacks[0].Outputs[] | select(.OutputKey | contains("RootUrl")).OutputValue'`
+BASE_URL=`aws cloudformation  describe-stacks --stack-name $2 | jq -r '.Stacks[0].Outputs[] | select(.OutputKey | contains("RootUrl")).OutputValue'`
 
-for i in `seq 1 $2`
+for i in `seq 1 $1`
 	do
 	txid=$RANDOM
 	curl -v $BASE_URL/bids?appuid=123\&txid=${txid}\&campaign_item_id=${CID}\&domain=example.com\&creative_category=test\&creative_id=some
