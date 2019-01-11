29839ff2eece3281f421da4ec229856c20900208
German Osin <german.osin@gmail.com>
2019-01-11 16:49:22 +0300

    refactoring

--- fds-it/src/test/java/com/provectus/fds/it/AwsFdsTestIT.java
+++ fds-it/src/test/java/com/provectus/fds/it/AwsFdsTestIT.java
@@ -31,6 +31,14 @@ import static org.junit.Assert.assertNotNull;
 public class AwsFdsTestIT {
     private static final String STACK_NAME_PREFIX = "fdsit";
     private static final String REGION = "us-west-2";
+
+    public static final String URL_FOR_API = "UrlForAPI";
+    public static final String URL_FOR_REPORTS = "UrlForReports";
+    public static final String BID_TYPE = "bid";
+    public static final String IMP_TYPE = "impression";
+    public static final String CLICK_TYPE = "click";
+    public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8";
+
     private static CloudFormation cloudFormation;
     private static String reportUrl;
     private static String apiUrl;
@@ -44,8 +52,8 @@ public class AwsFdsTestIT {
                 , String.format("%s_%s", STACK_NAME_PREFIX, UUID.randomUUID().toString())
                 , new File("fds.yaml")
         );
-        reportUrl = cloudFormation.getOutput("UrlForAPI").getOutputValue();
-        apiUrl = cloudFormation.getOutput("UrlForReports").getOutputValue();
+        reportUrl = cloudFormation.getOutput(URL_FOR_API).getOutputValue();
+        apiUrl = cloudFormation.getOutput(URL_FOR_REPORTS).getOutputValue();
     }
 
     @After
@@ -63,7 +71,10 @@ public class AwsFdsTestIT {
     public void testDynamoTotalReport() throws IOException, ExecutionException, InterruptedException {
         ThreadLocalRandom random = ThreadLocalRandom.current();
 
-        int length = 10;
+        int numberOfBids = random.nextInt(100, 200);
+        int numberOfImps = random.nextInt(numberOfBids/4, numberOfBids/2);
+        int numberOfClicks = random.nextInt(numberOfImps/4, numberOfImps/2);
+
         String domain = "www.google.com";
         String creativeCategory = "testCreativeCategory";
         String creativeId = UUID.randomUUID().toString();
@@ -71,7 +82,7 @@ public class AwsFdsTestIT {
 
         Map<String, List<ListenableFuture<Response>>> futuresByType = new HashMap<>();
 
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < numberOfBids; i++) {
 
             String txid = UUID.randomUUID().toString();
             String appuid = UUID.randomUUID().toString();
@@ -82,17 +93,25 @@ public class AwsFdsTestIT {
             ClickBcn clickBcn = new ClickBcn(txid, Instant.now().toEpochMilli());
 
 
-            futuresByType.computeIfAbsent("bid", (k) -> new ArrayList<>())
-                    .add(sendRequest("bid", bid));
-            futuresByType.computeIfAbsent("impression", (k) -> new ArrayList<>())
-                    .add(sendRequest("impression", impressionBcn));
-            futuresByType.computeIfAbsent("click", (k) -> new ArrayList<>())
-                    .add(sendRequest("click", clickBcn));
+            futuresByType.computeIfAbsent(BID_TYPE, (k) -> new ArrayList<>())
+                    .add(sendRequest(BID_TYPE, bid));
+
+            if (numberOfImps>0) {
+                futuresByType.computeIfAbsent(IMP_TYPE, (k) -> new ArrayList<>())
+                        .add(sendRequest(IMP_TYPE, impressionBcn));
+                numberOfImps--;
+            }
+
+            if (numberOfClicks>0) {
+                futuresByType.computeIfAbsent(CLICK_TYPE, (k) -> new ArrayList<>())
+                        .add(sendRequest(CLICK_TYPE, clickBcn));
+                numberOfClicks--;
+            }
         }
 
-        int countOfBids = awaitSuccessfull(futuresByType.get("bid"));
-        int countOfImpressions = awaitSuccessfull(futuresByType.get("impression"));
-        int countOfClicks = awaitSuccessfull(futuresByType.get("click"));
+        int countOfBids = awaitSuccessfull(futuresByType.get(BID_TYPE));
+        int countOfImpressions = awaitSuccessfull(futuresByType.get(IMP_TYPE));
+        int countOfClicks = awaitSuccessfull(futuresByType.get(CLICK_TYPE));
 
         awaitReport(campaignItemId, countOfBids, countOfImpressions, countOfClicks);
     }
@@ -125,7 +144,7 @@ public class AwsFdsTestIT {
 
     private Aggregation getReport(long campaignItemId) throws IOException, ExecutionException, InterruptedException {
         Response response = httpClient.prepareGet(reportUrl +"/reports/campaigns/"+campaignItemId)
-                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8")
+                .addHeader("User-Agent", USER_AGENT)
                 .execute()
                 .get();
 
@@ -135,7 +154,7 @@ public class AwsFdsTestIT {
     private <T> ListenableFuture<Response> sendRequest(String type, T model) throws JsonProcessingException {
         return httpClient.preparePost(String.format("%s/%s", apiUrl, type))
                 .addHeader("Content-Type", "application/json")
-                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8")
+                .addHeader("User-Agent", USER_AGENT)
                 .setBody(objectMapper.writeValueAsBytes(model))
                 .execute();
     }
