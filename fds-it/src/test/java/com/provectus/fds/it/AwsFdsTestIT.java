9c315ac08bd12b3ba3e492607d1e8b1b6bbf8172
German Osin <german.osin@gmail.com>
2019-01-11 16:41:31 +0300

    Improve IT test

--- fds-it/src/test/java/com/provectus/fds/it/AwsFdsTestIT.java
+++ fds-it/src/test/java/com/provectus/fds/it/AwsFdsTestIT.java
@@ -1,192 +1,143 @@
 package com.provectus.fds.it;
 
-import com.amazonaws.services.cloudformation.AmazonCloudFormation;
-import com.amazonaws.services.cloudformation.AmazonCloudFormationClientBuilder;
-import com.amazonaws.services.cloudformation.model.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.provectus.fds.dynamodb.models.Aggregation;
+import com.provectus.fds.it.aws.CloudFormation;
+import com.provectus.fds.models.bcns.Bid;
+import com.provectus.fds.models.bcns.ClickBcn;
+import com.provectus.fds.models.bcns.ImpressionBcn;
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.ListenableFuture;
+import org.asynchttpclient.Response;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import static org.asynchttpclient.Dsl.asyncHttpClient;
+import static org.asynchttpclient.Dsl.config;
+import static org.awaitility.Awaitility.await;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class AwsFdsTestIT {
-    private static String stackName = "fdsit";
-    private static String apiUrl;
+    private static final String STACK_NAME_PREFIX = "fdsit";
+    private static final String REGION = "us-west-2";
+    private static CloudFormation cloudFormation;
     private static String reportUrl;
-    private static String REGION = "us-west-2";
+    private static String apiUrl;
 
-    private static String convertStreamToString(InputStream in) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        StringBuilder stringbuilder = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            stringbuilder.append(String.format("%s\n", line));
-        }
-        in.close();
-        return stringbuilder.toString();
-    }
-
-    private static String waitForCompletion(AmazonCloudFormation stackbuilder) throws Exception {
-        DescribeStacksRequest wait = new DescribeStacksRequest();
-        wait.setStackName(stackName);
-        boolean completed = false;
-        String stackStatus = "Unknown";
-        String stackReason = "";
-        System.out.print("Waiting");
-        while (!completed) {
-            List<Stack> stacks = stackbuilder.describeStacks(wait).getStacks();
-            if (stacks.isEmpty()) {
-                completed = true;
-                stackStatus = "NO_SUCH_STACK";
-                stackReason = "Stack has been deleted";
-            } else {
-                for (Stack stack : stacks) {
-                    StackStatus currentStackStatus = StackStatus.valueOf(stack.getStackStatus());
-                    switch (currentStackStatus) {
-                        case CREATE_FAILED:
-                        case CREATE_COMPLETE:
-                        case ROLLBACK_FAILED:
-                        case ROLLBACK_COMPLETE:
-                        case DELETE_FAILED:
-                        case DELETE_COMPLETE:
-                            completed = true;
-                            stackStatus = stack.getStackStatus();
-                            stackReason = stack.getStackStatusReason();
-                            break;
-                        default:
-                            break;
-                    }
-                }
-            }
-            System.out.print(".");
-            if (!completed) Thread.sleep(10000);
-        }
-        System.out.println("done");
-        return String.format("%s (%s)", stackStatus, stackReason);
-    }
+    private final AsyncHttpClient httpClient = asyncHttpClient(config());
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
     public void beforeClass() throws Exception {
-        AmazonCloudFormation stackbuilder = AmazonCloudFormationClientBuilder.standard()
-                .withRegion(REGION)
-                .build();
-        CreateStackRequest createRequest = new CreateStackRequest();
-        createRequest.setStackName(stackName);
-        createRequest.setTemplateBody(convertStreamToString(new FileInputStream(new File("fds.yaml"))));
-        List<String> capabilities = Arrays.asList("CAPABILITY_IAM", "CAPABILITY_AUTO_EXPAND");
-        createRequest.setCapabilities(capabilities);
-        List<Parameter> parameters = Arrays.asList(
-                new Parameter()
-                        .withParameterKey("ServicePrefix")
-                        .withParameterValue(stackName),
-                new Parameter()
-                        .withParameterKey("AnalyticalDBName")
-                        .withParameterValue(stackName),
-                new Parameter()
-                        .withParameterKey("S3BucketName")
-                        .withParameterValue(String.format("fds%s", stackName))
+        cloudFormation = new CloudFormation(REGION
+                , String.format("%s_%s", STACK_NAME_PREFIX, UUID.randomUUID().toString())
+                , new File("fds.yaml")
         );
-        createRequest.setParameters(parameters);
-        stackbuilder.createStack(createRequest);
-        DescribeStacksResult describeStacksResult = stackbuilder.describeStacks(
-                new DescribeStacksRequest().withStackName(stackName)
-        );
-        System.out.println(String.format("Stack creation completed, the stack %s completed with %s",
-                stackName,
-                waitForCompletion(stackbuilder))
-        );
+        reportUrl = cloudFormation.getOutput("UrlForAPI").getOutputValue();
+        apiUrl = cloudFormation.getOutput("UrlForReports").getOutputValue();
     }
 
     @After
     public void afterClass() throws Exception {
-        AmazonCloudFormation stackbuilder = AmazonCloudFormationClientBuilder.standard()
-                .withRegion(REGION)
-                .build();
-        DeleteStackRequest deleteStackRequest = new DeleteStackRequest();
-        deleteStackRequest.setStackName(stackName);
-        stackbuilder.deleteStack(deleteStackRequest);
-        System.out.println(String.format("Stack deletion completed, the stack %s completed with %s",
-                stackName,
-                waitForCompletion(stackbuilder))
-        );
+        if (cloudFormation!=null) cloudFormation.close();
     }
 
     @Test
     public void testStackOutputs() {
-        AmazonCloudFormation stackbuilder = AmazonCloudFormationClientBuilder.standard()
-                .withRegion(REGION)
-                .build();
-        DescribeStacksResult describeStacksResult = stackbuilder.describeStacks(
-                new DescribeStacksRequest().withStackName(stackName));
-        for (Stack s : describeStacksResult.getStacks()) {
-            for (Output i : s.getOutputs()) {
-                if (i.getOutputKey().equalsIgnoreCase("UrlForAPI")) {
-                    apiUrl = i.getOutputValue();
-                }
-                if (i.getOutputKey().equalsIgnoreCase("UrlForReports")) {
-                    reportUrl = i.getOutputValue();
-                }
-            }
-        }
-        assert !apiUrl.isEmpty();
+        assertNotNull(reportUrl);
+        assertNotNull(apiUrl);
     }
 
     @Test
-    public void testAthenaReport() throws IOException {
+    public void testDynamoTotalReport() throws IOException, ExecutionException, InterruptedException {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
         int length = 10;
-        String appuid = "testAppuid";
-        String domain = "testDomain";
+        String domain = "www.google.com";
         String creativeCategory = "testCreativeCategory";
-        String creativeId = "testCreativeId";
-        int campaignItemId = 3;
-        int countOfBids = 0;
-        int countOfClicks = 0;
-        int countOfImpressions = 0;
+        String creativeId = UUID.randomUUID().toString();
+        long campaignItemId = random.nextLong(1_000_000L, 2_000_000L);
+
+        Map<String, List<ListenableFuture<Response>>> futuresByType = new HashMap<>();
+
         for (int i = 0; i < length; i++) {
-            int txid = campaignItemId * 100 + i;
-            String bidRequest =
-                    String.format("{\"win_price\": 1," +
-                            " \"appuid\": \"%s\"," +
-                            " \"campaign_item_id\": %d," +
-                            " \"creative_category\": \"%s\"," +
-                            " \"creative_id\":\"%s\"," +
-                            " \"txid\":\"%s\"," +
-                            " \"domain\":\"%s\"," +
-                            " \"type\":\"bid\"}", appuid, campaignItemId, creativeCategory, creativeId, txid, domain);
 
-            String clickRequest = String.format("{\"txid\":\"%s\", \"type\":\"click\"}", txid);
-            String impressionRequest = String.format("{\"txid\":\"%s\", \"win_price\": 1, \"type\":\"imp\"}", txid);
-            countOfBids += performRequest("bid", bidRequest);
-            countOfClicks += performRequest("click", clickRequest);
-            countOfImpressions += performRequest("impression", impressionRequest);
+            String txid = UUID.randomUUID().toString();
+            String appuid = UUID.randomUUID().toString();
+            long winPrice = random.nextInt(1_000, 2_000);
+
+            Bid bid = new Bid(txid,campaignItemId, domain,creativeId,creativeCategory, appuid);
+            ImpressionBcn impressionBcn = new ImpressionBcn(txid, Instant.now().toEpochMilli(),winPrice);
+            ClickBcn clickBcn = new ClickBcn(txid, Instant.now().toEpochMilli());
+
+
+            futuresByType.computeIfAbsent("bid", (k) -> new ArrayList<>())
+                    .add(sendRequest("bid", bid));
+            futuresByType.computeIfAbsent("impression", (k) -> new ArrayList<>())
+                    .add(sendRequest("impression", impressionBcn));
+            futuresByType.computeIfAbsent("click", (k) -> new ArrayList<>())
+                    .add(sendRequest("click", clickBcn));
         }
-        int[] report = getReport(campaignItemId);
-        assert countOfBids == report[0];
-        assert countOfClicks == report[1];
-        assert countOfImpressions == report[2];
+
+        int countOfBids = awaitSuccessfull(futuresByType.get("bid"));
+        int countOfImpressions = awaitSuccessfull(futuresByType.get("impression"));
+        int countOfClicks = awaitSuccessfull(futuresByType.get("click"));
+
+        awaitReport(campaignItemId, countOfBids, countOfImpressions, countOfClicks);
     }
 
-    private int[] getReport(int campaignItemId) throws IOException {
-        HttpURLConnection con = (HttpURLConnection) new URL(reportUrl +"/reports/campaigns/"+campaignItemId).openConnection();
-        con.setRequestMethod("GET");
-        con.setDoInput(true);
-        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8");
-        return new int[]{10, 10, 10};
+    private void awaitReport(long campaignItemId, int countOfBids, int countOfImpressions, int countOfClicks) {
+        await().atMost(15, TimeUnit.MINUTES)
+                .until( () -> {
+                    Aggregation report = getReport(campaignItemId);
+                    assertEquals(countOfBids,report.getBids());
+                    assertEquals(countOfImpressions, report.getImps());
+                    assertEquals(countOfClicks, report.getClicks());
+                    return true;
+                });
     }
 
-    private int performRequest(String type, String request) throws IOException {
-        HttpURLConnection con = (HttpURLConnection) new URL(apiUrl +"/"+type).openConnection();
-        con.setRequestMethod("POST");
-        con.setDoOutput(true);
-        con.setRequestProperty("Content-Type", "application/json");
-        con.setRequestProperty("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8");
-        OutputStream os = con.getOutputStream();
-        os.write(request.getBytes("UTF-8"));
-        os.flush();
-        return con.getResponseCode() == 200 ? 1 : 0;
+    private int awaitSuccessfull(List<ListenableFuture<Response>> futures) {
+        int n = 0;
+        for (ListenableFuture<Response> future : futures) {
+            try {
+                Response response = future.get();
+                if (response.getStatusCode()==200) {
+                    n++;
+                }
+            } catch (Throwable e) {
+
+            }
+        }
+        return n;
     }
+
+    private Aggregation getReport(long campaignItemId) throws IOException, ExecutionException, InterruptedException {
+        Response response = httpClient.prepareGet(reportUrl +"/reports/campaigns/"+campaignItemId)
+                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8")
+                .execute()
+                .get();
+
+        return objectMapper.readValue(response.getResponseBodyAsBytes(), Aggregation.class);
+    }
+
+    private <T> ListenableFuture<Response> sendRequest(String type, T model) throws JsonProcessingException {
+        return httpClient.preparePost(String.format("%s/%s", apiUrl, type))
+                .addHeader("Content-Type", "application/json")
+                .addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8")
+                .setBody(objectMapper.writeValueAsBytes(model))
+                .execute();
+    }
+
 }
