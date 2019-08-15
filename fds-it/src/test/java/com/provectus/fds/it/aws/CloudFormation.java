91729399ad50bd92b546a383b375d33f012fa58e
RustamGimadiev <gimadiev.kzn@yandex.ru>
2019-08-15 12:16:48 +0300

    fix s3bucket var retrieving

--- fds-it/src/test/java/com/provectus/fds/it/aws/CloudFormation.java
+++ fds-it/src/test/java/com/provectus/fds/it/aws/CloudFormation.java
@@ -73,7 +73,7 @@ public class CloudFormation implements AutoCloseable {
                 stackName,
                 result.getStackStatus()
         ));
-        s3bucket = getOutputsMap().get("Bucket".toLowerCase()).getOutputValue();
+        this.s3bucket = getOutput("Bucket".toLowerCase()).getOutputValue();
         return result;
     }
 
